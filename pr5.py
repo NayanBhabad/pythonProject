1f8a2493ddf4cfca7050756ff390476956f456b2
NayanBhabad <nayanbhabad179@gmail.com>
2021-03-04 10:01:22 +0530

    By using import math function find out the values of sine,cosine,tangent,logarithmic functions.etc

---
+++ pr5.py
@@ -0,0 +1,14 @@
+# The use of import math function.
+
+import math
+
+degree = float(input("Enter value of x ;"))
+a = 10
+pi = 22 / 7
+# x is the angle must be in radian
+x = (pi / 180) * degree
+print(math.sqrt(81))
+print(math.tan(x))
+print(math.log10(a))
+print(math.sin(x))
+print(math.cos(x))
