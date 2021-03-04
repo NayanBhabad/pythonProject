cf00a28a8bbcdf2881e4f4ea4313121fdf0837c2
NayanBhabad <nayanbhabad179@gmail.com>
2021-03-04 09:54:00 +0530

    Conversion of temperatures from Fahrenheit to Degree and vice versa.

---
+++ pr4.py
@@ -0,0 +1,11 @@
+# conversion of temp.- fahrenheit to degree celsius
+a = float(input("enter temp. in fahrenheit ;"))
+# temp. in degree celsius
+b = (5 / 9) * (a - 32)
+print(b)
+
+# conversion of temp.- degree celsius to fahrenheit
+c = float(input("enter temp. in degree celsius ;"))
+# temp. in fahrenheit
+d = (9 / 5) * c + 32
+print(d)
