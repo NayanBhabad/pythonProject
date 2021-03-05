47447a6200e96655aa54508f1bda682242d99b48
NayanBhabad <nayanbhabad179@gmail.com>
2021-03-05 12:39:39 +0530

    Use of all bitwise operators in python.

---
+++ pr10.py
@@ -0,0 +1,14 @@
+# All bitwise operators in python.
+'''& means and
+   | means or
+   ^ means biconditional
+   ~ means negation
+   (same in logical gates)'''
+
+x = 0
+y = 1
+print(x & y)
+print(x | y)
+print(x ^ y)
+print(~x)
+print(~y)
