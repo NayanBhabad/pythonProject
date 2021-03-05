3ac5cfae4f70c66efd08e8da583505d2ed8d2712
NayanBhabad <nayanbhabad179@gmail.com>
2021-03-05 12:36:22 +0530

    Use of all logical operators in python.

---
+++ pr9.py
@@ -0,0 +1,7 @@
+# All logical operators in python.
+
+x = float(input("Enter first number ;"))
+y = float(input("Enter second number ;"))
+print(x < 5 and y < 10)
+print(x > 5 or y <= 10)
+print(not x < 5 and y < 10)
