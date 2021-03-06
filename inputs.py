f613e1cb0e2ce39284bed04c9379785bb9006208
NayanBhabad <nayanbhabad179@gmail.com>
2021-03-06 09:21:15 +0530

    Taking multiple inputs from user in python.

---
+++ inputs.py
@@ -0,0 +1,10 @@
+# Taking multiple inputs from user in python.
+
+
+x, y, z = input("enter three values ;").split()
+x = float(x)
+y = float(y)
+z = float(z)
+print("total no. of students ;", x)
+print("no. of boys ;", y)
+print("no. of girls ;", z)
