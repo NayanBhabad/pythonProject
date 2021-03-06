e180fb1fba6b9ece0e4b7ea8da92ce44f972e474
NayanBhabad <nayanbhabad179@gmail.com>
2021-03-06 09:26:58 +0530

    Swapping of two values.

---
+++ pr11.py
@@ -0,0 +1,9 @@
+# Swapping of two values.
+
+x = 5
+y = 10
+pens = x
+x = y
+y = pens
+print("the value of x after swapiing is : {}".format(x))
+print("the value of y after swapiing is : {}".format(y))
