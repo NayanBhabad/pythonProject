b872bf4aa4f4c3e795981bb3d0afd306cfa51420
NayanBhabad <nayanbhabad179@gmail.com>
2021-03-06 09:25:58 +0530

    Use of 'import math as m' function.

---
+++ pr7.py
@@ -0,0 +1,14 @@
+# Use of 'import math as m' function.
+
+import math as m
+
+a = 36
+x = float(input("Enter the value of x ;"))
+# conversion of x in radian
+pi = 22 / 7
+y = (pi / 180) * x
+print(m.sqrt(a))
+print(m.cos(y))
+print(m.sin(y))
+print(m.tan(y))
+print(m.log(y, 10))
