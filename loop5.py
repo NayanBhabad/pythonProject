5b12e0d0f8746fe16bc7714d63a237a23fb83c7b
NayanBhabad <nayanbhabad179@gmail.com>
2021-03-09 11:26:32 +0530

    Check if a no. is a positive or a negative.

---
+++ loop5.py
@@ -0,0 +1,9 @@
+# Check if a no. is a positive or a negative.
+
+num = float(input('enter any value ;'))
+if num > 0:
+    print('positive')
+elif num == 0:
+    print('equal')
+else:
+    print('negative')
