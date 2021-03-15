a1715b8c3c459039aa3d6c714ac25bba3d5f200d
NayanBhabad <nayanbhabad179@gmail.com>
2021-03-15 16:54:45 +0530

    # Use of if,else,elif loop.

---
+++ loop.py
@@ -0,0 +1,26 @@
+# Use of if,else,elif loop.
+
+'''a = 5
+b = 10
+if (a>b):
+    print("a is greater")
+else :
+    print("b is greater")'''
+
+
+
+
+'''a = 2
+b = 5
+if a>b :
+    print("a is greater")
+elif a==b :
+    print("equal")
+else :
+    print("b is greater")'''
+
+# short hand if ..
+
+a = 25
+b = 10
+if a>b : print("a is greater than b ")
