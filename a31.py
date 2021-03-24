354d23bd6e829bd888880707587246522e201ff0
NayanBhabad <nayanbhabad179@gmail.com>
2021-03-24 13:24:56 +0530

    Division of two no.s such that division must prevented by zero.

---
+++ a31.py
@@ -0,0 +1,8 @@
+# Division of two no.s such that division must prevented by zero.
+
+a = int(input('enter first no. :'))
+b = int(input('enter second no. :'))
+if b != 0:
+    print(a / b)
+else:
+    print("Division is not possible..")
