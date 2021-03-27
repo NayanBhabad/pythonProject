bfdeba4ad7abe722f5139e95550c0e1b19bed91d
NayanBhabad <nayanbhabad179@gmail.com>
2021-03-27 19:05:52 +0530

    Check whether the alphabet enter by user is vowel or consonant.

---
+++ a36.py
@@ -0,0 +1,7 @@
+# Check whether the alphabet enter by user is vowel or consonant.
+
+a = input("enter a alphabet :")
+if a == 'A' or a == 'E' or a == 'I' or a == 'O' or a == 'U' or a == 'a' or a == 'e' or a == 'i' or a == 'o' or a == 'u':
+    print(a, "alphabet is vowel.")
+else:
+    print(a, "alphabet is consonant.")
